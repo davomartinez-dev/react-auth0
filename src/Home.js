bd32bcf1742bc8c7a9cd17f1ff0c4771c802b219
David Elì <davidelimg1@gmail.com>
2020-10-03 10:17:40 -0500

    Impplemented login button

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,13 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Home extends Component {
   render() {
-    return <h1>Home</h1>;
+    return (
+      <div>
+        <h1>Home</h1>
+        <button onClick={this.props.auth.login}>Login</button>
+      </div>
+    );
   }
 }
 
